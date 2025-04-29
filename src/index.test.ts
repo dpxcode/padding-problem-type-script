d277024b46667b313ba149773b95b42ff62af2e1
Durga Parshad <durga.parshad@nool.it>
2025-04-29 13:54:22 +0530

    Update test cases for number padding

--- src/index.test.ts
+++ src/index.test.ts
@@ -16,8 +16,12 @@ describe('checkNumberPadding', () => {
 
     test('should return -3 for inconclusive padding with length 3', () => {
         expect(checkNumberPadding(['999', '9999'])).toBe(-3);
+    });
+
+    test('should return -4 for inconclusive padding with length 4', () => {
         expect(checkNumberPadding(['9999', '99999'])).toBe(-4);
     });
+
 
     test('should return -2 for inconclusive padding with length 2', () => {
         expect(checkNumberPadding(['99', '999', '9999'])).toBe(-2);
